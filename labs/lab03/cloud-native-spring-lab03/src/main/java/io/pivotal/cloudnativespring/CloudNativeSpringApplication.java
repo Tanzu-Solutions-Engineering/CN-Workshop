c566faf215dd25438cd8fad702eb299afcdc0584
Subhankarc79 <subhankar.chatterjee@cgi.com>
2018-08-22 19:11:05 -0700

    Updated with Modified Lab03 and Lab04 Code

--- labs/lab03/cloud-native-spring-lab03/src/main/java/io/pivotal/cloudnativespring/CloudNativeSpringApplication.java
+++ labs/lab03/cloud-native-spring-lab03/src/main/java/io/pivotal/cloudnativespring/CloudNativeSpringApplication.java
@@ -1,6 +1,5 @@
 package io.pivotal.cloudnativespring;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // Add these imports:
--- labs/lab04/cloud-native-spring-lab04/src/main/java/io/pivotal/cloudnativespring/CloudNativeSpringApplication.java
+++ labs/lab04/cloud-native-spring-lab04/src/main/java/io/pivotal/cloudnativespring/CloudNativeSpringApplication.java
@@ -2,7 +2,9 @@ package io.pivotal.cloudnativespring;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 // Add these imports:
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,24 +12,29 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguratio
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @SpringBootApplication
 @RestController
 @EnableJpaRepositories // <---- And this
 @Import(RepositoryRestMvcConfiguration.class) // <---- And this
+
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class }) // To Bypass Security in the Demo Application
+
 public class CloudNativeSpringApplication {
-    @Value("${greeting:Hola}")
-    private String greeting;
+
+	@Value("${greeting:Hola}")
+	private String greeting;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudNativeSpringApplication.class, args);
 	}
 
-
-    @RequestMapping("/")
-    public String hello() {
-        return greeting + " World!";
-    }
-
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello World!";
 	}
+
+	@RequestMapping("/home")
+	public String hellohome() {
+		return "Hello World - Home!";
+	}
+}
