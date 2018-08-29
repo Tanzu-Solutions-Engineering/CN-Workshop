a2f81da44bfe69cf448c00835db91326e7f78b55
Subhankarc79 <subhankar.chatterjee@cgi.com>
2018-08-29 01:26:10 -0700

    Updated Lab06 with UI Client and Support for Feign & Hystrix

---
+++ labs/lab06/cloud-native-spring-lab06/src/main/java/io/pivotal/cloudnativespring/domain/City.java
@@ -0,0 +1,95 @@
+package io.pivotal.cloudnativespring.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "city")
+public class City implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id", updatable = false, nullable = false)
+	private long id;
+	
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String county;
+
+	@Column(nullable = false, name="state_code")
+	private String stateCode;
+
+	@Column(nullable = false, name="postal_code")
+	private String postalCode;
+
+	@Column
+	private String latitude;
+
+	@Column
+	private String longitude;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getStateCode() {
+		return stateCode;
+	}
+
+	public void setStateCode(String stateCode) {
+		this.stateCode = stateCode;
+	}
+
+	public String getCounty() {
+		return county;
+	}
+
+	public void setCounty(String county) {
+		this.county = county;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+}
---
+++ labs/lab06/cloud-native-spring-ui-lab06/src/main/java/io/pivotal/cloudnativespringui/CloudNativeSpringUiApplication.java
@@ -0,0 +1,44 @@
+package io.pivotal.cloudnativespringui;
+
+import java.util.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.Resources;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@EnableFeignClients // <---- Add this
+@EnableDiscoveryClient // <---- And this
+@EnableCircuitBreaker
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class }) // To Bypass Security in the Demo Application
+
+public class CloudNativeSpringUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeSpringUiApplication.class, args);
+	}
+
+	@FeignClient(name = "cloud-native-spring-lab06", fallback = CityClientFallback.class)
+	public interface CityClient {
+
+		@GetMapping(value = "/cities", consumes = "application/hal+json")
+		Resources<City> getCities();
+	}
+
+	@Component
+	public class CityClientFallback implements CityClient {
+		@Override
+		public Resources<City> getCities() {
+			// We'll just return an empty response
+			return new Resources<City>(Collections.emptyList());
+		}
+	}
+}
